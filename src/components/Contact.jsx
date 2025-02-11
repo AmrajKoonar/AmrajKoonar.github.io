130f30ed660f128b068adf0ab909c96472825597
AmrajKoonar <amrajk31@gmail.com>
2025-02-11 15:36:13 -0800

    style: Added mini discription for contact, added Resume Icon as well.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import githubIMG from '../assets/social_imgs/github_icon.svg';
 import linkedinIMG from '../assets/social_imgs/LinkedIn_icon.svg';
 import emailIMG from '../assets/social_imgs/email_logo.png';
+import resumeIMG from '../assets/social_imgs/resume_icon.png';
 
 const Contact = () => {
 
@@ -21,6 +22,11 @@ const Contact = () => {
         name: 'Email',
         src: emailIMG,
         link: 'mailto:ask36@sfu.ca'
+    },
+    {
+        name: 'Resume',
+        src: resumeIMG,
+        link: '#'
     }
     ];
 
@@ -50,6 +56,24 @@ const Contact = () => {
           Contact me!
         </motion.h2>
 
+        <motion.p 
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          style={{
+            textAlign: 'center',
+            maxWidth: '600px',
+            margin: '0 auto 3rem',
+            fontSize: '1.1rem',
+            lineHeight: '1.8'
+          }}
+        >
+          I'm always interested in hearing about new opportunities, collaborations,
+          or just having a chat about technology and development. Feel free to reach
+          out through any of the platforms below!
+        </motion.p>
+
+        
 
 
 
