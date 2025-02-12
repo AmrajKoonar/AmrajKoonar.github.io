d405c45fa2353182f500544d8ab82a9553730678
AmrajKoonar <amrajk31@gmail.com>
2025-02-11 16:57:24 -0800

    feat: Created Resume.jsx, and set up Router, npm installed router as well in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Projects from './components/Projects'
-import Courses from './components/Courses'
-import Contact from './components/Contact'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Courses from './components/Courses';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <About />
-      <Projects />
-      <Courses />
-      <Contact />
-    </>
-  )
+    <Router>
+      <Navbar /> 
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Home />
+            <About />
+            <Projects />
+            <Courses />
+            <Contact />
+          </>
+        } />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
